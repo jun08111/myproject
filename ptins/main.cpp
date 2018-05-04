8e90320c819196af924e2034e3211b318b7c3f08
jun08111@naver.com <root@localhost.localdomain>
2018-05-04 09:34:58 +0900

    WIPS FakeAP

--- ptins/main.cpp
+++ ptins/main.cpp
@@ -1,68 +1,53 @@
 #include <tins/tins.h>
-#include <cassert>
 #include <iostream>
+#include <cassert>
 #include <vector>
 using namespace Tins;
 using namespace std;
-/*
-int main()
+
+bool addr1234(PDU& packet)
 {
-    vector<Packet> vt;
-    SnifferConfiguration config;
-    config.set_filter("port 80");
-    config.set_promisc_mode(true);
-    config.set_snap_len(400);
-    Sniffer sniffer("wlan1", config);
-    unique_ptr<PDU> pdu_ptr(sniffer.next_packet());
+    const Dot11Data& dot11  = packet.rfind_pdu<Dot11Data>();
+    const ARP& arp  = packet.rfind_pdu<ARP>();
 
-    while (vt.size() != 100)
+    if (dot11.from_ds() && !dot11.to_ds())
     {
-        vt.push_back(sniffer.next_packet()); //next_packet returns a PtrPacket, which can be implicitly converted to Packet.
-    }
+        cout << "addr1 BSSID: " << dot11.addr1() <<endl
+             << "addr2 Src:   " << dot11.addr2() <<endl
+             << "addr3 Dst:   " << dot11.addr3() <<endl
+             << "Sender Mac:   " << arp.sender_hw_addr() <<endl
+             << "Target Mac:   " << arp.target_hw_addr() <<endl
 
-    for (const auto& packet : vt)
-    {
-        cout << "Dst Mac:  "  << packet.pdu()->rfind_pdu<EthernetII>().dst_addr() <<endl
-             << "Src Mac:  "  << packet.pdu()->rfind_pdu<EthernetII>().src_addr() <<endl
-             << "Src addr: " << packet.pdu()->rfind_pdu<IP>().src_addr() << endl
-             << "Dst addr: " << packet.pdu()->rfind_pdu<IP>().dst_addr() << endl
-             << "Src port: " << packet.pdu()->rfind_pdu<TCP>().sport() <<endl
-             << "Dst port: " << packet.pdu()->rfind_pdu<TCP>().dport() <<endl
-             << endl;
-
-    }
-
-}
-*/
-bool handler(const Packet&)
-{
-    vector<Packet> vt;
-    SnifferConfiguration config;
-    config.set_filter("port 80");
-    config.set_promisc_mode(true);
-    Sniffer sniffer("wlan0",config);
-
-    vt.push_back(sniffer.next_packet()); //next_packet returns a PtrPacket, which can be implicitly converted to Packet.
-
-    for (const auto& packet : vt)
-    {
-        cout << "Dst Mac:  " << packet.pdu()->rfind_pdu<EthernetII>().dst_addr() <<endl
-             << "Src Mac:  " << packet.pdu()->rfind_pdu<EthernetII>().src_addr() <<endl
-             << "Src addr: " << packet.pdu()->rfind_pdu<IP>().src_addr() << endl
-             << "Dst addr: " << packet.pdu()->rfind_pdu<IP>().dst_addr() << endl
-             << "Src port: " << packet.pdu()->rfind_pdu<TCP>().sport() <<endl
-             << "Dst port: " << packet.pdu()->rfind_pdu<TCP>().dport() <<endl
              << endl;
     }
+
+    else if (!dot11.from_ds() && dot11.to_ds())
+    {
+        cout << "addr1 Dst:   " << dot11.addr1() <<endl
+             << "addr2 BSSID: " << dot11.addr2() <<endl
+             << "addr3 Src:   " << dot11.addr3() <<endl
+             << "Sender Mac:   " << arp.sender_hw_addr() <<endl
+             << "Target Mac:   " << arp.target_hw_addr() <<endl
+             << endl;
+    }
+
     return true;
 }
 
 int main()
 {
-    Sniffer sniffer("wlan0");
-    //auto decrypt_proxy = Crypto::make_wpa2_decrypter_proxy(&handler);
-    //decrypt_proxy.decrypter().add_ap_data("jeon0926","Jeon");
-    sniffer.sniff_loop(&handler);
-    //sniffer.sniff_loop(decrypt_proxy);
+    SnifferConfiguration config;
+    config.set_promisc_mode(true);
+    config.set_immediate_mode(true);
+    Sniffer sniffer("wlan1", config);
+
+    auto decrypt_proxy = Crypto::make_wpa2_decrypter_proxy(&addr1234);
+    decrypt_proxy.decrypter().add_ap_data("information", "833");
+    sniffer.sniff_loop(decrypt_proxy);
+
     return 0;
 }
+
+//const ARP& arp  = packet.rfind_pdu<ARP>();
+//<< "Sender Mac:   " << arp.sender_hw_addr() <<endl
+//<< "Target Mac:   " << arp.target_hw_addr() <<endl
--- ptins/ptins.pro
+++ ptins/ptins.pro
@@ -8,3 +8,5 @@ SOURCES += \
 
 LIBS += -lpcap
 LIBS += -ltins
+
+HEADERS +=
