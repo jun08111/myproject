1118260b5607d2a131da4d8cd0ed0a2b4212afd2
jun08111@naver.com <root@localhost.localdomain>
2018-06-03 11:06:03 +0900

    pcap

--- pcap/main.cpp
+++ pcap/main.cpp
@@ -21,10 +21,12 @@ void ether_header(const u_int8_t *);
 void ip_header(const u_int8_t *);
 void tcp_header(const u_int8_t *);
 
-void ether_header(const u_int8_t *packet){
+void ether_header(const u_int8_t *packet)
+{
     eh = (struct ether_header *)packet;
     printf("*Ethernet Header");
-    for(int i=0; i<12; i++){
+    for(int i=0; i<12; i++)
+    {
         if     (i==0) printf("\nDst Mac Address: %02x ", packet[i]);
         else if(i==6) printf("\nSrc Mac Address: %02x ", packet[i]);
         else          printf("%02x ", packet[i]);
@@ -32,9 +34,11 @@ void ether_header(const u_int8_t *packet){
     printf("\n");
 }
 
-void ip_header(const u_int8_t *packet){
+void ip_header(const u_int8_t *packet)
+{
     packet += sizeof(struct ether_header);
-    if (ntohs(eh->ether_type) == ETHERTYPE_IP){
+    if (ntohs(eh->ether_type) == ETHERTYPE_IP)
+    {
         iph = (struct iphdr *)packet;
         char buf[INET_ADDRSTRLEN];
         printf("*IP Header\n");
@@ -43,13 +47,16 @@ void ip_header(const u_int8_t *packet){
     }
 }
 
-void tcp_header(const u_int8_t *packet){
+void tcp_header(const u_int8_t *packet)
+{
     packet += iph->ihl * 4;
     tcph = (struct tcphdr *) packet;
-    if(iph->protocol == IPPROTO_TCP){
+    if(iph->protocol == IPPROTO_TCP)
+    {
         printf("*TCP Header\n");
         printf("Src Port: %d\n", ntohs(tcph->th_sport));
         printf("Dst Port: %d\n", ntohs(tcph->th_dport));
+
         int paylen = int(ntohs(iph->tot_len) - (iph->ihl * 4) - (tcph->th_off * 4));
         printf("paylen: %d\n", paylen);
 
@@ -57,7 +64,8 @@ void tcp_header(const u_int8_t *packet){
         payload =(u_int8_t *)(packet + (tcph->th_off * 4));
         printf("payload hexa Value\n");
         int cl = 0;
-        while(paylen--){
+        while(paylen--)
+        {
             printf("%02x ", *(payload++));
             if((++cl % 16) == 0) printf("\n");
         }
@@ -65,7 +73,7 @@ void tcp_header(const u_int8_t *packet){
     }
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     char *dev;
     pcap_t *handle;
@@ -77,34 +85,46 @@ int main()
     bpf_u_int32 net;
     bpf_u_int32 mask;
 
+    if (argc != 2)
+    {
+        cout << "Usage: " <<* argv << " <device>" << endl;
+        return 1;
+    }
+
     dev = pcap_lookupdev(errbuf);
-    if (dev == NULL) {
+    if (dev == NULL)
+    {
         fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
         return 2;
     }
 
     handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-    if (handle == NULL) {
+    if (handle == NULL)
+    {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return 2;
     }
 
-    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1){
+    if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1)
+    {
         fprintf(stderr, "Couldn't get netmask for device %s: %s\n", dev, errbuf);
         return 2;
     }
 
-    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
+    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1)
+    {
         fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return 2;
     }
 
-    if (pcap_setfilter(handle, &fp) == -1) {
+    if (pcap_setfilter(handle, &fp) == -1)
+    {
         fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return 2;
     }
 
-    while(int result = pcap_next_ex(handle, &header, &packet) >=0){
+    while(int result = pcap_next_ex(handle, &header, &packet) >=0)
+    {
         if      (result == 0)  continue;      //Timeout expired, There is no packet.
         else if (result == -1) break;         //-1: error,Signal Lost
         else if (result == -2)                //-2: EOF, No more packet from the packet savefile.
@@ -112,53 +132,13 @@ int main()
             fprintf(stderr, "No more packet from the packet savefile.");
             break;
         }
+
         ether_header(packet);
         ip_header(packet);
         tcp_header(packet+sizeof(struct ether_header));
 
-/* not use function
-        eh = (struct ether_header *)packet;
-        printf("*Ethernet Header\n");
-        printf("Dst Mac Address: ");
-        for(int i=0; i<6; i++){
-            printf("%02x ", eh->ether_dhost[i]);
-        }
-        printf("\nSrc Mac Address: ");
-        for(int i=0; i<6; i++){
-            printf("%02x ", eh->ether_shost[i]);
-        }
-        printf("\n");
+        printf("\n\n");
 
-        packet += sizeof(struct ether_header);
-        char buf[INET_ADDRSTRLEN];
-        if (ntohs(eh->ether_type) == ETHERTYPE_IP){
-            iph = (struct iphdr *)packet;
-            printf("*IP Header\n");
-            printf("Src IP Addr: %s\n", inet_ntop(AF_INET,&iph->saddr, buf, sizeof(buf))); //inet_ntop: binary -> human-readable text
-            printf("Dst IP Addr: %s\n", inet_ntop(AF_INET,&iph->daddr, buf, sizeof(buf)));
-        }
-
-        packet += iph->ihl * 4;
-        if(iph->protocol == IPPROTO_TCP){
-            tcph = (struct tcphdr *) packet;
-            printf("*TCP Header\n");
-            printf("Src Port: %d\n", ntohs(tcph->th_sport));
-            printf("Dst Port: %d\n", ntohs(tcph->th_dport));
-
-            int paylen;
-            paylen = int(ntohs(iph->tot_len) - (iph->ihl * 4) - (tcph->th_off * 4));
-            printf("paylen: %d\n", paylen);
-
-            payload =(u_char *)(packet + (tcph->th_off * 4));
-            printf("payload hexa Value\n");
-            int cl = 0;
-            while(paylen--){
-                printf("%02x ", *(payload++));
-                if((++cl % 16) == 0) printf("\n");
-            }
-            printf("\n\n");
-        }
-*/
     }
     pcap_close(handle);
     return 0;
